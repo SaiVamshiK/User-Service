ceeb67461802ff245c97416bf2c21e8c7781124a
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-02-08 17:37:34 +0530

    minor changes

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -5,10 +5,7 @@ import com.example.userservice.VO.ResponseObject;
 import com.example.userservice.entity.User;
 import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -19,15 +16,15 @@ public class UserController {
 
     @Autowired
     private RestTemplate restTemplate;
-    @RequestMapping("/")
+    @PostMapping("/")
     public User saveUser(@RequestBody User user){
         return userService.saveUser(user);
     }
 
-    @RequestMapping("/{userID}")
+    @GetMapping("/{userID}")
     public ResponseObject getUserAndDepartmentByUserId(@PathVariable("userID") String userId){
         User user = userService.getUserByID(Long.valueOf(userId));
-        Department department = restTemplate.getForObject("http://localhost:8081/departments"+String.valueOf(user.getDepartmentId()),
+        Department department = restTemplate.getForObject("http://localhost:8081/departments/"+String.valueOf(user.getDepartmentId()),
                 Department.class);
         ResponseObject responseObject = new ResponseObject();
         responseObject.setUser(user);
